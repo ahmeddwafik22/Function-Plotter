296df8845cfbf8f89783867d043ac9a68d59726c
ahmeddwafik22 <60493730+ahmeddwafik22@users.noreply.github.com>
2023-07-18 01:34:31 -0700

    Add files via upload

---
+++ test_preprocessed_input.py
@@ -0,0 +1,31 @@
+import pytest
+import eval_system
+
+@pytest.mark.parametrize("fun_x, expected_validation" ,[
+    ("2","2"),
+    ("2x","2*x"),
+    ("2X","2*x"),
+    ("2.x","2.0*x"),
+    ("2,x","2.0*x"),
+    ("2.5(x+2)","2.5*(x+2)"),
+    ("2.+x","2.0+x"),
+    ("2^x","2**x"),
+    ("+2x","+2*x"),
+    ("-2x","-2*x"),
+    ("2x**2","2*x**2"),
+    ("2x^2","2*x**2"),
+    ("2x+++2","2*x+2"),
+    ("2x---2","2*x-2"),
+    ("x2","x*2"),
+    ("(2+x)8","(2+x)*8"),
+    ("[3x+3]2","(3*x+3)*2"),
+    ("[3x*(2-1)]","(3*x*(2-1))"),
+    ("{3x*(2-1)}","(3*x*(2-1))"),
+    ("2x(4+8)","2*x*(4+8)"),
+])
+
+def test(fun_x, expected_validation):
+    o=eval_system.eval_service()
+    v,s=o.preprocess_text(fun_x)
+    print(s)
+    assert s == expected_validation
